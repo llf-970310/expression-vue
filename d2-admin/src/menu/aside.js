b70acd482c6b7f4eabcfe9181584e81e8bc9273d
CharlesFeng <fdfengjunjie@126.com>
2019-03-18 20:58:51 +0800

    Revert "修复url异常的问题"
    This reverts commit 13c10c2c

--- d2-admin/src/menu/aside.js
+++ d2-admin/src/menu/aside.js
@@ -1,24 +1,13 @@
 // 菜单 侧边栏
 export default [
-    {
-        path: '/index', title: '首页', icon: 'home'
-    },
+    {path: '/index', title: '首页', icon: 'home'},
     {
         title: '管理员功能',
         icon: 'folder-o',
         children: [
-            {
-                path: '/admin/account',
-                title: '账户管理'
-            },
-            {
-                path: '/admin/question',
-                title: '题目管理'
-            },
-            {
-                path: '/admin/score',
-                title: '成绩管理'
-            }
+            {path: '/account', title: '账户管理'},
+            {path: '/question', title: '题目管理'},
+            {path: '/score', title: '成绩管理'},
         ]
     }
 ]
