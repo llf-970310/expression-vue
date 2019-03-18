13c10c2cd9a7a1d0e8935ae01272721f1b0907a3
yueyong1030@outlook.com <yueyong1030@outlook.com>
2019-03-18 18:46:22 +0800

    修复url异常的问题

--- d2-admin/src/menu/aside.js
+++ d2-admin/src/menu/aside.js
@@ -1,13 +1,24 @@
 // 菜单 侧边栏
 export default [
-    {path: '/index', title: '首页', icon: 'home'},
+    {
+        path: '/index', title: '首页', icon: 'home'
+    },
     {
         title: '管理员功能',
         icon: 'folder-o',
         children: [
-            {path: '/account', title: '账户管理'},
-            {path: '/question', title: '题目管理'},
-            {path: '/score', title: '成绩管理'},
+            {
+                path: '/admin/account',
+                title: '账户管理'
+            },
+            {
+                path: '/admin/question',
+                title: '题目管理'
+            },
+            {
+                path: '/admin/score',
+                title: '成绩管理'
+            }
         ]
     }
 ]
