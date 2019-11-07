08c23a0edecdb1026b336b3681f08b55322ba57e
dylanchu <chdy.uuid@gmail.com>
2019-11-08 02:15:45 +0800

    conf:微信登录重定向:expression.iselab.cn->www.parclabcn.com

--- src/pages/login/page.vue
+++ src/pages/login/page.vue
@@ -175,7 +175,7 @@
         // let redirectUrl = '/api/auth/wechat/login'
         location.href = 'https://open.weixin.qq.com/connect/qrconnect?' +
           'appid=wxd7bad9aab33bb581&' +
-          'redirect_uri=https://expression.iselab.cn' + redirectUrl + '&' +
+          'redirect_uri=https://www.parclabcn.com' + redirectUrl + '&' +
           'response_type=code&' +
           'scope=snsapi_login&' +
           'state=zidingyineirong#wechat_redirect'
